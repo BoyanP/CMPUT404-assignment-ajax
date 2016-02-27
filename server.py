6b5268fdf88fb00d90f5d962b3f69ff06acf37ce
BoyanP <bgpeychoff@gmail.com>
2016-02-27 16:12:02 -0700

    server and routing seems to work

--- server.py
+++ server.py
@@ -22,8 +22,9 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request , send_from_directory
 import json
+
 app = Flask(__name__)
 app.debug = True
 
@@ -74,27 +75,38 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    print str(myWorld.world())
+    return flask.redirect('/static/index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    jsonData = flask_post_json()
+
+    if request.method == 'PUT':
+        myWorld.set(entity,jsonData)
+
+    if request.method =="POST":
+        for key in jsonData.keys():
+            myWorld.update(entity,key,jsonData[key])
+
+    result = myWorld.get(entity)
+    return json.dumps(result) 
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
-
+    return flask.jsonify(myWorld.world())
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
